5ce4de68f5e4131ccbbfd861915ee40f076e07d5
raihanrizqifauzan <raihanrizqif@gmail.com>
2020-12-10 22:50:11 +0700

    Database Seeding with Faker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\Model\Product::class, 25)->create();
+        factory(App\Model\Review::class, 100)->create();
     }
 }
